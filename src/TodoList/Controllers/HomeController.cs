ec6a77d814a3c2e8ea0529dce0654d1f94cb79ea
Iaroslav_Rudchenko <Iaroslav_Rudchenko@epam.com>
2017-01-13 22:14:10 +0300

    Task2

--- src/TodoList/Controllers/HomeController.cs
+++ src/TodoList/Controllers/HomeController.cs
@@ -1,5 +1,12 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Polly;
 using TodoList.Context;
 using TodoList.Enum;
 using TodoList.ViewModel;
@@ -10,13 +17,19 @@ namespace TodoList.Controllers
     public class HomeController : Controller
     {
         private TaskListContext _context;
-
+        private readonly List<string> _apiList;
         public HomeController(TaskListContext context)
         {
             _context = context;
+            _apiList = new List<string>
+            {
+                "http://10.23.22.111/cardsboard/api/cards" ,
+                "http://10.23.22.119/core/api/CardApi",
+                "http://10.23.22.118/oleksii_budianskyi/todomanager/api/tasks"
+            };
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             TaskTodoListViewModel viewModel = new TaskTodoListViewModel
             {
@@ -26,6 +39,32 @@ namespace TodoList.Controllers
             };
 
             return View(viewModel);
+        }
+
+        private async Task Method(TaskTodoListViewModel viewModel)
+        {
+            foreach (var link in _apiList)
+            {
+                using (var httpclient = new HttpClient())
+                {
+                    var respond = await httpclient.GetStringAsync(link);
+                    var todoList = JsonConvert.DeserializeObject<TaskTodo[]>(respond);
+
+                    viewModel.LisTaskTodosStatusDone =
+                        viewModel.LisTaskTodosStatusDone.Concat(todoList.Where(m => m.Status == Status.Done))
+                            .ToList();
+
+                    viewModel.LisTaskTodosStatusInprogress =
+                        viewModel.LisTaskTodosStatusInprogress.Concat(
+                                todoList.Where(m => m.Status == Status.InProgress))
+                            .ToList();
+
+                    viewModel.LisTaskTodosStatusPlanned =
+                        viewModel.LisTaskTodosStatusPlanned.Concat(
+                                todoList.Where(m => m.Status == Status.Planned))
+                            .ToList();
+                }
+            }
         }
 
         [HttpPost]
